ac1eb09dfaecb4f7a756f41e503baa7e65a499b2
josemigueldev <josemiguel.developer@gmail.com>
2021-08-31 00:02:34 -0500

    02 - frames con pack

---
+++ 02_frame_pack.py
@@ -0,0 +1,13 @@
+import tkinter
+
+ventana = tkinter.Tk()
+
+marco1 = tkinter.Frame()
+marco1.config(width="400", height="200", bg="lightgreen")
+marco1.pack()
+
+marco2 = tkinter.Frame()
+marco2.config(width="400", height="200", bg="lightblue")
+marco2.pack()
+
+ventana.mainloop()
